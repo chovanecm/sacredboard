9b779732f42bd8576c56287c2cb8ce444b70ca8c
Martin Chovanec <chovanecm@gmail.com>
2016-11-13 21:44:25 +0100

    Added click to allow running Flask in debug mode

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,12 +1,16 @@
 from flask import Flask
-
+import click
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello World!'
+    return "Hello world"
+
+@click.command()
+@click.option("--debug", is_flag=True, default=False)
+def run(debug):
+    app.run(host="0.0.0.0", debug=debug)
 
 
 if __name__ == '__main__':
-    app.run()
+    run()
