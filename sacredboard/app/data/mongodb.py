1625ce6e9a4d2b2753eb12687f7fb82d472cbb33
Martin Chovanec <chovanecm@gmail.com>
2017-02-23 17:01:16 +0100

    Add the option to select the collection containing the run entries (#20)
    https://github.com/chovanecm/sacredboard/issues/20

--- sacredboard/app/data/mongodb.py
+++ sacredboard/app/data/mongodb.py
@@ -5,19 +5,32 @@ import pymongo
 
 class PyMongoDataAccess:
     """ Access records in MongoDB. """
-    def __init__(self, host, port, db):
+    def __init__(self, host, port, db, collection_name):
+        """
+
+        :param host: The database server to connect to
+        :type host: str
+        :param port: Database port
+        :type port: int
+        :param db: Database name
+        :type db: str
+        :param collection_name: Name of the collection
+         where Sacred stores its runs
+        :type collection_name: str
+        """
         self._host = host
         self._port = port
         self._db_name = db
         self._client = None
         self._db = None
+        self._collection_name = collection_name
 
     def connect(self):
         self._client = pymongo.MongoClient(host=self._host, port=self._port)
         self._db = getattr(self._client, self._db_name)
 
     def get_runs(self, sort_by=None, sort_direction=None, start=0, limit=None):
-        cursor = self._db.runs.find()
+        cursor = getattr(self._db, self._collection_name).find()
         if sort_by is not None:
             cursor = self._apply_sort(cursor, sort_by, sort_direction)
         cursor = cursor.skip(start)
@@ -27,10 +40,12 @@ class PyMongoDataAccess:
 
     def get_run(self, run_id):
         try:
-            cursor = self._db.runs.find({"_id": int(run_id)})
+            cursor = getattr(self._db, self._collection_name)\
+                .find({"_id": int(run_id)})
         except ValueError:
             # Probably not a number.
-            cursor = self._db.runs.find({"_id": bson.ObjectId(run_id)})
+            cursor = getattr(self._db, self._collection_name)\
+                .find({"_id": bson.ObjectId(run_id)})
         run = None
         for c in cursor:
             run = c
