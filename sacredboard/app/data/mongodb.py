e2fa62596be89d58333f127f3bc769fa00e1beed
Martin Chovanec <chovamar@fit.cvut.cz>
2017-03-01 15:20:38 +0100

    Add the -mu option to pass MongoDB URI
    As requested in #21
    https://github.com/chovanecm/sacredboard/issues/21
    Added the "-mu" option to pass a connection string as defined in
    https://docs.mongodb.com/manual/reference/connection-string/
    
    As a result, it's possible to connect to password-protected databases etc.

--- sacredboard/app/data/mongodb.py
+++ sacredboard/app/data/mongodb.py
@@ -5,28 +5,17 @@ import pymongo
 
 class PyMongoDataAccess:
     """ Access records in MongoDB. """
-    def __init__(self, host, port, db, collection_name):
-        """
 
-        :param host: The database server to connect to
-        :type host: str
-        :param port: Database port
-        :type port: int
-        :param db: Database name
-        :type db: str
-        :param collection_name: Name of the collection
-         where Sacred stores its runs
-        :type collection_name: str
-        """
-        self._host = host
-        self._port = port
-        self._db_name = db
+    def __init__(self, uri, database_name, collection_name):
+
+        self._uri = uri
+        self._db_name = database_name
         self._client = None
         self._db = None
         self._collection_name = collection_name
 
     def connect(self):
-        self._client = pymongo.MongoClient(host=self._host, port=self._port)
+        self._client = pymongo.MongoClient(host=self._uri)
         self._db = getattr(self._client, self._db_name)
 
     def get_runs(self, sort_by=None, sort_direction=None, start=0, limit=None):
@@ -40,18 +29,19 @@ class PyMongoDataAccess:
 
     def get_run(self, run_id):
         try:
-            cursor = getattr(self._db, self._collection_name)\
+            cursor = getattr(self._db, self._collection_name) \
                 .find({"_id": int(run_id)})
         except ValueError:
             # Probably not a number.
-            cursor = getattr(self._db, self._collection_name)\
+            cursor = getattr(self._db, self._collection_name) \
                 .find({"_id": bson.ObjectId(run_id)})
         run = None
         for c in cursor:
             run = c
         return run
 
-    def _apply_sort(self, cursor, sort_by, sort_direction):
+    @staticmethod
+    def _apply_sort(cursor, sort_by, sort_direction):
         """
         :type sort_direction: str
         :return:
@@ -62,3 +52,33 @@ class PyMongoDataAccess:
         else:
             sort = pymongo.ASCENDING
         return cursor.sort(sort_by, sort)
+
+    @staticmethod
+    def build_data_access(host, port, database_name, collection_name):
+        """
+       :param host: The database server to connect to
+       :type host: str
+       :param port: Database port
+       :type port: int
+       :param database_name: Database name
+       :type database_name: str
+       :param collection_name: Name of the collection
+        where Sacred stores its runs
+       :type collection_name: str
+               """
+        return PyMongoDataAccess("mongodb://%s:%d" % (host, port),
+                                 database_name, collection_name)
+
+    @staticmethod
+    def build_data_access_with_uri(uri, database_name, collection_name):
+        """
+       :param uri: Connection string as defined in
+                https://docs.mongodb.com/manual/reference/connection-string/
+       :type uri: str
+       :param database_name: Database name
+       :type database_name: str
+       :param collection_name: Name of the collection
+        where Sacred stores its runs
+       :type collection_name: str
+               """
+        return PyMongoDataAccess(uri, database_name, collection_name)
