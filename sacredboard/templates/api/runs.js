cf73b38f9eab9f3d2941e864040cab6a4613020e
Martin Chovanec <chovanecm@gmail.com>
2017-04-25 20:44:03 +0200

    Remove captured output last line from the API
    no longer needed

--- sacredboard/templates/api/runs.js
+++ sacredboard/templates/api/runs.js
@@ -14,7 +14,7 @@
         "heartbeat": {{run.heartbeat | default | format_datetime | tojson}},
         "heartbeat_diff": {{run.heartbeat | default | timediff | tojson}},
         "hostname": {{run.host.hostname | default | tojson}},
-        "captured_out_last_line": {{run.captured_out | default | last_line | tojson}},
+        {# commented out: "captured_out_last_line": {{run.captured_out | default | last_line | tojson}}, #}
         "result":{{run.result | default | tojson}}
         {%- if full_object -%},
         "object": {{run | dump_json}}
