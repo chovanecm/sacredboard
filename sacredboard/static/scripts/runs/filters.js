94a2df8ba2178080c0293ef3d786cf296b3ef8b4
Martin Chovanec <chovanecm@gmail.com>
2017-04-27 17:38:02 +0200

    Modify filtering to comply with #8

--- sacredboard/static/scripts/runs/filters.js
+++ sacredboard/static/scripts/runs/filters.js
@@ -9,7 +9,47 @@ define("runs/filters", ["knockout", "knockoutMapping", "jquery", "text!runs/filt
          * @constructor
          */
         function QueryFilter(field, operator, value) {
-            this.field = ko.observable(field);
+            var self = this;
+            /* In accordance with https://github.com/chovanecm/sacredboard/issues/8
+             the user searches by default in the config scope.
+             He may, however, search everywhere if he prepends "." to the beginning
+             of the query, e.g. ".host.hostname"
+             */
+            var defaultScope = "config.";
+            this.userInputField = ko.observable("");
+            // Search in config. or in the whole run?
+            this.fieldScope = ko.pureComputed(function () {
+                if (self.userInputField().startsWith(".")) {
+                    return "";
+                } else {
+                    return defaultScope;
+                }
+            }, this);
+            this.field = ko.pureComputed({
+                read: function () {
+                    if (self.userInputField().startsWith(".")) {
+                        return self.userInputField().substring(1);
+                    } else if (self.userInputField() != "") {
+                        return self.fieldScope() + self.userInputField();
+                    } else {
+                        return "";
+                    }
+                },
+                /**
+                 * @param value String
+                 */
+                write: function (value) {
+                    if (value.startsWith(defaultScope)) {
+                        // Set the user input field without the default scope name
+                        self.userInputField(value.substring(defaultScope.length));
+                    } else if (value != "") {
+                        self.userInputField("." + value);
+                    } else {
+                        self.userInputField("");
+                    }
+                }
+            });
+            this.field(field);
             this.operator = ko.observable(operator);
             this.value = ko.observable(value).extend({validateJSONValue: this});
             this.nativeValue = ko.pureComputed(function () {
@@ -21,7 +61,11 @@ define("runs/filters", ["knockout", "knockoutMapping", "jquery", "text!runs/filt
             };
 
             this.toDto = function () {
-                return ko.mapping.toJS({"field": this.field(), "operator": this.operator(), "value": this.nativeValue()});
+                return ko.mapping.toJS({
+                    "field": this.field(),
+                    "operator": this.operator(),
+                    "value": this.nativeValue()
+                });
             };
 
             this.addParentObserver = function (observer) {/* Do nothing, we consider a QueryFilter to be used as immutable object */
@@ -82,11 +126,17 @@ define("runs/filters", ["knockout", "knockoutMapping", "jquery", "text!runs/filt
                     return fieldAndOperatorSet && !this.filterToAdd().value.hasError();
                 };
 
-                this.filterToAdd().field.subscribe(function () {
-                    if (this.filterToAdd().field().startsWith(".")) {
-                        this.filterToAdd().field("config" + this.filterToAdd().field());
-                    }
-                }, this);
+                // this.filterToAdd().field.subscribe(function () {
+                //     /**
+                //      * To comply with Use Case UC 1 (described in Martin Chovanec's Master's thesis)
+                //      * the user search by default in the config. scope
+                //      */
+                //     if (this.filterToAdd().field().startsWith(".")) {
+                //         this.filterToAdd().fieldScope("");
+                //     } else {
+                //         this.filterToAdd().fieldScope("config.");
+                //     }
+                // }, this);
             },
             template: htmlTemplate
         });
