15a256954e68bb8f98b28436019228b6dfee5227
Martin Chovanec <chovanecm@gmail.com>
2017-05-04 02:09:45 +0200

    Set experiment config to be expandable (prototype)

--- sacredboard/static/scripts/runs/dictionaryBrowser.js
+++ sacredboard/static/scripts/runs/dictionaryBrowser.js
@@ -3,11 +3,18 @@ define(["knockout", "jquery", "escapeHtml", "text!runs/dictionaryBrowser.html"],
     function (ko, $, escapeHtml, htmlTemplate) {
         ko.components.register("dictionary-browser", {
             viewModel: function (params) {
-                this.dict = new DictEntry("", params.value);
                 this.escape = escapeHtml;
+                this.dict = new DictEntry("", params.value);
 
                 function DictEntry(name, value) {
                     var self = this;
+                    this.value = value;
+                    this.contentCollapsed = ko.observable(true);
+                    this.toggleCollapse = function () {
+                        if (self.hasChildren()) {
+                            self.contentCollapsed(!self.contentCollapsed());
+                        }
+                    };
                     this.hasChildren = function () {
                         return typeof value == "object";
                     };
@@ -27,12 +34,15 @@ define(["knockout", "jquery", "escapeHtml", "text!runs/dictionaryBrowser.html"],
                     };
                     this.getDisplayValue = function () {
                         if (value instanceof Array) {
-                            return value.join(", ");
+                            return "[" + value.join(", ") + "]";
                         } else {
                             return value.toString();
                         }
                     };
+
+                    this.content = value;
                 }
+                DictEntry.prototype.EMPTY = "";
             },
             template: htmlTemplate
         });
