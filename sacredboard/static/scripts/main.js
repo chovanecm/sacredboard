ef7e16697d42d0ee91513dac8a45a5789ae441b4
Martin Chovanec <chovanecm@gmail.com>
2017-05-04 11:54:18 +0200

    Attempt to fix problem with DataTables module loading
    DataTables seems to replace other modules in require(["abc"], function(abc) {
      //abc for some reason often (not always) referes to DataTables :-/
    });

--- sacredboard/static/scripts/main.js
+++ sacredboard/static/scripts/main.js
@@ -4,11 +4,11 @@ var require = {
     paths: {
         jquery: "../vendors/jquery/jquery-3.1.1.min",
         knockout: "../vendors/knockout/knockout-min",
-        datatables: "../vendors/datatables/datatables.min",
+        "datatable": "../vendors/datatables/datatables.min",
         "datatables.net": "../vendors/datatables/DataTables-1.10.13/js/jquery.dataTables",
         "datatables-bootstrap" : "../vendors/datatables/DataTables-1.10.13/js/dataTables.bootstrap",
         bootstrap: "../vendors/bootstrap/js/bootstrap.min",
-        text: "../vendors/text"
+        "text": "../vendors/text"
 
     },
     shim : {
--- sacredboard/static/scripts/runs/dictionaryBrowser/component.js
+++ sacredboard/static/scripts/runs/dictionaryBrowser/component.js
@@ -1,7 +1,6 @@
 "use strict";
-define(["knockout", "jquery", "escapeHtml", "text!runs/dictionaryBrowser/template.html",
-    "runs/dictionaryBrowser/DictEntry"],
-    function (ko, $, escapeHtml, htmlTemplate, DictEntry) {
+define(["knockout", "escapeHtml", "runs/dictionaryBrowser/DictEntry", "text!runs/dictionaryBrowser/template.html"],
+    function (ko, escapeHtml, DictEntry, htmlTemplate) {
         ko.components.register("dictionary-browser", {
             viewModel: function (params) {
                 this.escape = escapeHtml;
