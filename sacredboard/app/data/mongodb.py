299eeba2bf47b9bc933bc5754504cbea901e2c8e
Martin Chovanec <chovanecm@gmail.com>
2017-05-21 12:21:28 +0200

    Add docstrings to mongodb.py Do not check code style for "tests" (although better it would be better to check them too)

--- sacredboard/app/data/mongodb.py
+++ sacredboard/app/data/mongodb.py
@@ -61,9 +61,9 @@ class PyMongoDataAccess:
         (one of ``"=="``, ``"!="``, ``"<"``, ``"<="``, ``">``, ``">="``,
         and ``"regex"`` for regular expressions).
         The ``value`` field contains the value to be compared with
-        (either a string or a number).
-        Notice that for the ``status`` field, the ``RUNNING`` and ``DEAD`` runs
-        are compared by :func:`~PyMongoDataAccess.RUNNING_DEAD_RUN_CLAUSE` and
+        (either a string or a number). Notice that for the ``status`` field,
+        the ``RUNNING`` and ``DEAD`` runs are compared by
+        :func:`~PyMongoDataAccess.RUNNING_DEAD_RUN_CLAUSE` and
         :func:`~PyMongoDataAccess.RUNNING_NOT_DEAD_CLAUSE`
         """
         mongo_query = self._to_mongo_query(query)
@@ -78,7 +78,7 @@ class PyMongoDataAccess:
     def get_run(self, run_id):
         """
         Get a single run from the database.
-        
+
         :param run_id: The ID of the run.
         :return: The whole object from the database.
         """
@@ -98,11 +98,11 @@ class PyMongoDataAccess:
     def _apply_sort(cursor, sort_by, sort_direction):
         """
         Apply sort to a cursor.
-        
+
         :param cursor: The cursor to apply sort on.
         :param sort_by: The field name to sort by.
         :param sort_direction: The direction to sort, "asc" or "desc".
-        :return: 
+        :return:
         """
         if sort_direction is not None and sort_direction.lower() == "desc":
             sort = pymongo.DESCENDING
@@ -114,7 +114,7 @@ class PyMongoDataAccess:
     def _to_mongo_query(query):
         """
         Convert the query received by the Sacred Web API to a MongoDB query.
-        
+
         Takes a query in format
         {"type": "and", "filters": [
          {"field": "host.hostname", "operator": "==", "value": "ntbacer"},
@@ -149,7 +149,7 @@ class PyMongoDataAccess:
     def _simple_clause_to_query(clause):
         """
         Convert a clause from the Sacred Web API format to the MongoDB format.
-        
+
         :param clause: A clause to be converted. It must have "field",
         "operator" and "value" fields.
         :return: A MongoDB clause.
@@ -159,7 +159,7 @@ class PyMongoDataAccess:
         value = clause["value"]
         if clause["field"] == "status" and \
            clause["value"] in ["DEAD", "RUNNING"]:
-            return PyMongoDataAccess.\
+            return PyMongoDataAccess. \
                 _status_filter_to_query(clause)
         if clause["operator"] == "==":
             mongo_clause[clause["field"]] = value
@@ -181,13 +181,13 @@ class PyMongoDataAccess:
     def _status_filter_to_query(clause):
         """
         Convert a clause querying for an experiment state RUNNING or DEAD.
-        
+
         Queries that check for experiment state RUNNING and DEAD need to be
         replaced by the logic that decides these two states as both of them
         are stored in the Mongo Database as "RUNNING". We use querying by last
         heartbeat time.
-        
-        :param clause: A clause whose field is "status" and "value" is one of 
+
+        :param clause: A clause whose field is "status" and "value" is one of
         RUNNING, DEAD.
         :return: A MongoDB clause.
         """
@@ -202,29 +202,32 @@ class PyMongoDataAccess:
     @staticmethod
     def build_data_access(host, port, database_name, collection_name):
         """
-       :param host: The database server to connect to
-       :type host: str
-       :param port: Database port
-       :type port: int
-       :param database_name: Database name
-       :type database_name: str
-       :param collection_name: Name of the collection
-        where Sacred stores its runs
-       :type collection_name: str
-               """
+        Create data access gateway.
+
+        :param host: The database server to connect to.
+        :type host: str
+        :param port: Database port.
+        :type port: int
+        :param database_name: Database name.
+        :type database_name: str
+        :param collection_name: Name of the collection with Sacred runs.
+        :type collection_name: str
+        """
         return PyMongoDataAccess("mongodb://%s:%d" % (host, port),
                                  database_name, collection_name)
 
     @staticmethod
     def build_data_access_with_uri(uri, database_name, collection_name):
         """
-       :param uri: Connection string as defined in
+        Create data access gateway given a MongoDB URI.
+
+        :param uri: Connection string as defined in
                 https://docs.mongodb.com/manual/reference/connection-string/
-       :type uri: str
-       :param database_name: Database name
-       :type database_name: str
-       :param collection_name: Name of the collection
+        :type uri: str
+        :param database_name: Database name
+        :type database_name: str
+        :param collection_name: Name of the collection
         where Sacred stores its runs
-       :type collection_name: str
-               """
+        :type collection_name: str
+        """
         return PyMongoDataAccess(uri, database_name, collection_name)
