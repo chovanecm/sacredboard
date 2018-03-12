fcdf42436be2a93b5b6a7f04fd36665ab36d6895
Martin Chovanec <chovanecm@gmail.com>
2018-03-12 22:36:29 +0100

    Reformat comments

--- sacredboard/app/sacredboard.py
+++ sacredboard/app/sacredboard.py
@@ -1,12 +1,14 @@
 # coding=utf-8
 """Module for handling core functions linked to sacredboard itself."""
 
-import pkg_resources # used to fetch the version number
+import pkg_resources  # used to fetch the version number
+
 
 class sacredboard():
+    """Class related to sacredboard itself. A better comment will follow later."""
 
     @staticmethod
     def get_version():
-        """Returns the current version of sacredboard."""
+        """Return the current version of sacredboard."""
         version = pkg_resources.require("sacredboard")[0].version
         return version
