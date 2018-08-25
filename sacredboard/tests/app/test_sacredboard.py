b91d1fd1d083b6d54d25def525ef9b9e603f5ab3
Martin Chovanec <chovanecm@gmail.com>
2018-08-25 14:08:13 +0200

    Increase dev version to 0.4.3.dev1

--- sacredboard/tests/app/test_sacredboard.py
+++ sacredboard/tests/app/test_sacredboard.py
@@ -5,4 +5,4 @@ from sacredboard.app.sacredboard import Sacredboard
 
 class TestSacredboard(TestCase):
     def test_get_version(self):
-        assert Sacredboard.get_version() == "0.4.2"
+        assert Sacredboard.get_version() == "0.4.3.dev1"
